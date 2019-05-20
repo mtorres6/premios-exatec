8c116991c13123a237f61251b4f2d0aa333e1452
matfirst <marcos.torres@definityfirst.com>
2019-05-20 10:24:34 -0600

    exatec - Select arrow interaction fixed

--- js/main.js
+++ js/main.js
@@ -2,9 +2,26 @@ jQuery(window).on('load', function () {
     $('.carousel').carousel({
         interval: 2000
     });
-    
-    $('.custom-select').click(function(){
-        $(this).toggleClass('opened');
+
+    /* Select arrow control  */ 
+    var open = 0;
+    $('.custom-select').blur(function(){
+        if(open != 0) {
+            $('.custom-select').removeClass('opened');
+            open = 0;
+        } 
     });
 
+    $('.custom-select').click(function(){
+        if(open == 0) {
+            $('.custom-select').addClass('opened');
+            open = 1;
+        } else {
+            $('.custom-select').removeClass('opened');
+            open = 0;
+        }
+    });
+    
 });
+
+
