ddcde41491cd706dcb7c14eff5edff501fcc4642
Pau-liz <paulina.a.mendoza@gmail.com>
2019-07-13 12:55:30 -0600

    Creación interna questions

---
+++ questions.php
@@ -0,0 +1,83 @@
+<?php include('header.php'); ?>
+
+    <div class="questions">
+        <div class="banner banner-simple">
+            <div class="bg-title">
+                <div class="container-fluid">
+                    <div class="row">
+                        <div class="col-sm-8 offset-sm-2">
+                            <h1 class="title">Preguntas frecuentes</h1>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="block-txt">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-8 offset-1">
+                        <p class="title1">¿Quién puede nominar?</p>
+                        <p class="txt">Cualquier persona, fisica o moral, es bienvenida para realizar su nominación
+                        a cualquiera de las disticiones de los Premios Exatec.
+                        </p>
+
+                        <p class="title1">¿Puedo autonominarme?</p>
+                        <p class="txt">No, la nominación debe ser recibida por un tercero (persona física o moral). Si consideras que cuentas con el perfil y trayectoria para ganar un Premio EXATEC, pide a alguien conocido que te nomine.</p>
+
+                        <p class="title1">¿Puedo nominar a mi candidato a ambas distinciones?</p>
+                        <p class="txt">¡Claro! Si no tienes claro la mejor distinción a la cual nominar a tu candidato postúlalo a ambas distinciones.</p>
+
+                        <p class="title1">¿Cuál es el periodo de nominación?</p>
+                        <p class="txt">Las fechas de nominación se encuentran disponibles en la página oficial.</p>
+
+                        <p class="title1">¿Puedo nominar a alguien si ya se cerró la convocatoria?</p>
+                        <p class="txt">No. Las nominaciones para </p>
+
+                        <p class="title1">¿Cualquier persona puede ser nominada?</p>
+                        <p class="txt">Todos los nominados deberán ser egresados del Tecnológico de Monterrey.</p>
+
+                        <p class="title1">¿Dónde puedo realizar la nominación?</p>
+                        <p class="txt">A través de la página de egresados.tec.mx /Premios EXATEC, a través del formato de nominación en línea.</p>
+
+                        <p class="title1">¿Cuántas postulaciones puedo realizar?</p>
+                        <p class="txt">No hay límite en el número de postulaciones a realizar, puedes nominar a todo EXATEC que cumpla con los criterios de nominación.</p>
+
+                        <p class="title1">¿Se aceptan postulaciones póstumas?</p>
+                        <p class="txt">Si, no mayor a 5 años de fallecido el nominado.</p>
+
+                        <p class="title1">¿Quién conforma el jurado?</p>
+                        <p class="txt">El jurado para cada distinción se integra por miembros de la Institución, representantes o líderes EXATEC e invitados de la comunidad. </p>
+
+                        <p class="title1">¿Qué reciben los ganadores?</p>
+                        <p class="txt">Cada ganador recibe:</p>
+                        <ul>
+                            <li class="txt">Escultura distintiva “Trayectoria EXATEC”.</li>
+                            <li class="txt">Reconocimiento escrito y enmarcado (tamaño título) por parte de las autoridades del Tecnológico de Monterrey. </li>
+                        </ul>
+                                           
+                        <p class="title1">¿Mi postulación puede ser anónima?</p>
+                        <p class="txt">Sí, dentro del formulario de postulación puedes seleccionar la opción para que tu nombre no se divulgue en el proceso. Sin embargo, deberás proporcionarnos tus datos para fines de consultas referentes a tu candidato nominado. </p>
+
+                        <p class="title1">¿Mi postulado puede ganar en ambas distinciones?</p>
+                        <p class="txt">No, durante el proceso de evaluación y selección de ganadores se identificará la distinción que mejor represente al postulado. Un mismo candidato no puede ganar ambas distinciones en el mismo año, sin embargo, puede ganar una de ellas y seguir participando para la otra. </p>
+                    
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="banner banner-with-txt">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-sm-6 offset-3">
+                        <h2 class="title1">¿No resolvimos tu duda?</h2>
+                        <h3 class="title2">Escribe un correo a la siguiente dirección y nosotros te contactaremos.</h3>
+                        <p class="title3">premios.exatec.@tec.mx</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+<?php include('footer.php'); ?>
