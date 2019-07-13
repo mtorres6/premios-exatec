f993ef66426379e8ff2f4f2bc473470b34894b45
matfirst <marcos.torres@definityfirst.com>
2019-07-13 17:38:06 -0600

    Secciones del home creadas

--- index.php
+++ index.php
@@ -1,3 +1,20 @@
 <?php include('header.php'); ?>
+    <div class="home">
+        <div class="nominate-exatec"></div>
+    
+        <div class="awards-exatec"></div>
+    
+        <div class="banner banner-small"></div>
+    
+        <div class="distinctions"></div>
+    
+        <div class="campus-participants"></div>
+    
+        <div class="testimonial"></div>
+    
+        <div class="tips"></div>
+    
+        <div class="banner banner-with-button"></div>
+    </div>
 
 <?php include('footer.php'); ?>
