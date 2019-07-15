b188ef82e33dd753bfca487190894a73c6a9b8cc
Pau-liz <paulina.a.mendoza@gmail.com>
2019-07-14 18:21:35 -0600

    Creación de las primeras secciones del home

--- index.php
+++ index.php
@@ -1,8 +1,45 @@
 <?php include('header.php'); ?>
     <div class="home">
-        <div class="nominate-exatec"></div>
+        <div class="nominate-exatec">
+            <div class="banner first-banner">
+                <a href="#">
+                    <div class="button-first-banner button-general">
+                        <div class="container-fluid">
+                            <div class="row">
+                                <div class="col-sm-9 offset-sm-2">
+                                    <p class="title">Nomina a un EXATEC</p>
+                                    <hr class="line">
+                                    <i class="fas fa-chevron-right"></i>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <img class="icon-scroll" src="assets/img/Icon-Scrol.svg" alt="">
+                </a>
+            </div>
+        </div>
     
-        <div class="awards-exatec"></div>
+        <div class="awards-exatec">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-4 offset-1">
+                        
+                        <img class="bg-img" src="assets/img/awards-EXATEC.jpg" alt="Premios">
+                    </div>
+                    <div class="col-6">
+                        <div class="box-txt">
+                            <p class="first-title">Premios EXATEC</p>
+                            <hr class="line1">
+                            <p class="first-txt">El Tecnológico de Monterrey busca reconocer y vincular a los egresados
+                                con la Institución, despertando en ellos y en la comunidad Tec el sentido de orgullo, 
+                                pertenencia y amor hacia su alma máter, reconociendo y visibilizando el mérito y trayectorias
+                                extraordinarias de EXATEC.  
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     
         <div class="banner banner-small">
             <div class="container-banner-txt">
@@ -10,7 +47,68 @@
             </div>
         </div>
     
-        <div class="distinctions"></div>
+        <div class="distinctions">
+            <div class="container-fluid padd-right">
+                <div class="row">
+                    <div class="col-6 offset-3">
+                        <div class="box-title">
+                            <p class="txt1">Distinciones</p>
+                            <hr class="line3">
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-4 offset-1">
+                        <div class="block-title">
+                            <p class="txt">Trayectoria EXATEC</p>
+                        </div>
+                    </div>
+                    <div class="col-7">
+                        <img class="img1" src="assets/img/Distinciones.jpg" alt="">
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-8">
+                        <div class="box-text">
+                            <p class="text">Egresados que de forma sostenida han destacado extraordinariamente, fortaleciendo positivamente la reputación y prestigio del Tec, de manera nacional y/o internacional, con alto grado de responsabilidad social, en distintas disciplinas como: empresarial, educación, ciencia e investigación, función pública, derechos humanos, voluntariado, filantropía, artes o deportes. </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="block-two">
+                <div class="container-fluid sin-padd">
+                    <div class="row">
+                        <div class="col-7">
+                            <img class="img2" src="assets/img/Merito-EXATEC.jpg" alt="">
+                        </div>
+                        <div class="col-5">
+                            <div class="box-text2">
+                                <p class="text2">Egresados que en su quehacer profesional han impactado positivamente al Tec y en su comunidad. Esta distinción se otorga a través de campus, regiones o escuelas de graduados que participen en las convocatorias.</p>
+                                <div class="row">
+                                    <div class="col-10 offset-2">
+                                        <a href="#">
+                                            <p class="text-win">Ver ganadores</p>
+                                            <hr class="line-win">
+                                            <i class="fas fa-chevron-right"></i>
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="row">
+                        <div class="col-4 offset-3">
+                            <div class="block-title">
+                                <p class="txt">Mérito EXATEC</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            
+        </div>
     
         <div class="campus-participants"></div>
     
