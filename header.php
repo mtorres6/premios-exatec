6163f5434e61a817ae88206aeace24bbf70f7420
Pau-liz <paulina.a.mendoza@gmail.com>
2019-07-28 09:46:02 -0600

    Actualización menu movil, responsive y banner principal

--- header.php
+++ header.php
@@ -34,16 +34,20 @@
           </div>
           <div class="col-lg-8 offset-lg-1 col-sm-12"> 
             <div class="menu">
-              <!--nav class="navbar navbar-expand-lg navbar-expand-sm navbar-light">
-                          
-                  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+              <nav class="navbar navbar-expand-lg navbar-light justify-content-sm-start">
+                  <a class="navbar-brand hidden order-1 order-lg-0 ml-lg-0 ml-2 mr-auto" href="">Brand</a>
+                  <button class="navbar-toggler align-self-start" type="button">
                     <span class="icon-bar"></span>
                     <span class="icon-bar size-line"></span>
                     <span class="icon-bar"></span>
                   </button>
-
-                  <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                      <ul class="navbar-nav">
+            
+                  <div class="collapse navbar-collapse d-flex flex-column flex-lg-row flex-xl-row justifity-content-lg-end p-3 p-lg-0 mt-lg-0 mobileMenu" id="navbarSupportedContent">
+                    <div class="d-block d-lg-none d-xl-none">
+                      <i class="fas fa-times"></i>
+                      <div class="logo" id="logo-movil"></div>
+                    </div>    
+                      <ul class="navbar-nav align-self-stretch">
 
                           <li class="nav-item">
                               <a class="nav-link" id="txt-menu" href="#">Premios Exatec</a>
@@ -66,8 +70,9 @@
                           </li>                                    
                       </ul>
                   </div>
-              </nav-->
-              <nav class="navbar navbar-expand-lg navbar-dark">
+                </nav>
+                <div class="overlay"></div>
+              <!--nav class="navbar navbar-expand-lg navbar-dark">
                 <a class="navbar-brand hidden" href="">Brand</a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
                   <span class="icon-bar"></span>
@@ -97,7 +102,7 @@
                     </li>     
                   </ul>
                 </div>
-              </nav>
+              </nav-->
               <div class="btn-movil d-block d-lg-none d-xl-none">
                 <a id="txt-menu" class="nav-link button-general button-primary button-menu-header" href="#">Nomina a un Exatec</a>
               </div>
